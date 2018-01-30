1d6554f5fe629760d925260d11bba116d779b190
dayiethan <30461116+dayiethan@users.noreply.github.com>
2018-01-29 20:37:24 -0600

    Cubetake arm edit
    added methods to cubetake arm to lift up and drop down

--- src/org/usfirst/frc/team1306/robot/subsystems/CubetakeArm.java
+++ src/org/usfirst/frc/team1306/robot/subsystems/CubetakeArm.java
@@ -1,6 +1,8 @@
 package org.usfirst.frc.team1306.robot.subsystems;
 
 import org.usfirst.frc.team1306.robot.RobotMap;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.command.Subsystem;
@@ -8,11 +10,27 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 public class CubetakeArm extends Subsystem {
 
 	private TalonSRX talon;
-	private DigitalInput limitOne;
-	private DigitalInput limitTwo;
+	private DigitalInput limittop;
+	private DigitalInput limitbottom;
 	
 	public CubetakeArm() {
 		talon = new TalonSRX(RobotMap.CUBETAKEARM_PORT);
+		limittop = new DigitalInput(RobotMap.CUBETAKEARM_LIMITTOP);
+		limitbottom = new DigitalInput(RobotMap.CUBETAKEARM_LIMITBOTTOM);
+	}
+	
+	public void ArmUp() {
+		if (limittop.get() == true) {
+			talon.set(ControlMode.PercentOutput, 0);
+		}
+		talon.set(ControlMode.PercentOutput, 0.2);
+	}
+	
+	public void ArmDown() {
+		if (limitbottom.get() == true) {
+			talon.set(ControlMode.PercentOutput, 0);
+		}
+		talon.set(ControlMode.PercentOutput, -0.2);
 	}
 
 	@Override
