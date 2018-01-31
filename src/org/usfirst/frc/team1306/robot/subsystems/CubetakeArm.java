cfc396ac07d823fa495bd1681681a6263f4d2880
dayiethan <30461116+dayiethan@users.noreply.github.com>
2018-01-30 23:08:38 -0600

    Robot Arm
    with pneumatics this time

--- src/org/usfirst/frc/team1306/robot/subsystems/CubetakeArm.java
+++ src/org/usfirst/frc/team1306/robot/subsystems/CubetakeArm.java
@@ -1,38 +1,27 @@
 package org.usfirst.frc.team1306.robot.subsystems;
-
 import org.usfirst.frc.team1306.robot.RobotMap;
-
-import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
-import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class CubetakeArm extends Subsystem {
 
 	private TalonSRX talon;
-	private DigitalInput limittop;
-	private DigitalInput limitbottom;
+	private DoubleSolenoid solenoid;
 	
 	public CubetakeArm() {
 		talon = new TalonSRX(RobotMap.CUBETAKEARM_PORT);
-		limittop = new DigitalInput(RobotMap.CUBETAKEARM_LIMITTOP);
-		limitbottom = new DigitalInput(RobotMap.CUBETAKEARM_LIMITBOTTOM);
+		solenoid = new DoubleSolenoid(RobotMap.CUBETAKE_CHANNEL_ONE_PORT, RobotMap.CUBETAKE_CHANNEL_TWO_PORT);
 	}
 	
-	public void ArmUp() {
-		if (limittop.get() == true) {
-			talon.set(ControlMode.PercentOutput, 0);
-		}
-		talon.set(ControlMode.PercentOutput, 0.2);
-	}
-	
-	public void ArmDown() {
-		if (limitbottom.get() == true) {
-			talon.set(ControlMode.PercentOutput, 0);
-		}
-		talon.set(ControlMode.PercentOutput, -0.2);
+	public void CubetakeArmUp() {
+		solenoid.set(DoubleSolenoid.Value.kReverse);
 	}
 
+	public void CubetakeArmDown() {
+		solenoid.set(DoubleSolenoid.Value.kForward);
+	}
+	
 	@Override
 	protected void initDefaultCommand() {
 		
